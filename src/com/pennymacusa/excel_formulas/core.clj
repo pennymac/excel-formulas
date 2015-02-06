989f1b808b7a1b944da35e2499049e2cb86fb3fe
Daniel Dyba <daniel.dyba@pnmac.com>
2015-02-06 15:07:23 -0800

    Modifies 360days function signature
    * 360days function must accept date-time objects rather than strings representing dates.

--- src/com/pennymacusa/excel_formulas/core.clj
+++ src/com/pennymacusa/excel_formulas/core.clj
@@ -5,22 +5,22 @@
 
 (defn days360
   "Calculates the number of days between a start-date and end-date according to
-  a 360-day year. Expects a string formatted as a date."
+  a 360-day year. Expects date-time objects."
   [start-date end-date]
-  (let [[sd ed] (let [d1 (f/parse start-date)
-                      d2 (f/parse end-date)]
-                  (if (t/after? d1 d2) [d2 d1] [d1 d2]))
-        [sdy sdm sdd] [(t/year sd) (t/month sd) (t/day sd)]
-        [edy edm edd] [(t/year ed) (t/month ed) (t/day ed)]
-        sdd (if (p/last-day-of-month? sd)
-              30
-              sdd)
-        [edm edd] (if (and (p/last-day-of-month? ed)
-                           (if (p/february? sd)
-                             (< (t/day sd) 29)
-                             (< (t/day sd) 30)))
-                    [(+ 1 (t/month ed)) 1]
-                    [(t/month ed) (min 30 (t/day ed))])]
+  (let [[sd ed] (if (t/after? start-date end-date)
+                  [end-date start-date]
+                  [start-date end-date])
+        [sdy sdm sdd] (let [day (if (p/last-day-of-month? sd)
+                                  30
+                                  (t/day sd))]
+                        [(t/year sd) (t/month sd) day])
+        [edy edm edd] (let [[month day] (if (and (p/last-day-of-month? ed)
+                                                 (if (p/february? sd)
+                                                   (< (t/day sd) 29)
+                                                   (< (t/day sd) 30)))
+                                          [(+ 1 (t/month ed)) 1]
+                                          [(t/month ed) (min 30 (t/day ed))])]
+                        [(t/year ed) month day])]
     (+ (* 360 (- edy sdy))
        (* 30 (- edm sdm))
        (- edd sdd))))
--- test/com/pennymacusa/excel_formulas/core_test.clj
+++ test/com/pennymacusa/excel_formulas/core_test.clj
@@ -1,22 +1,23 @@
 (ns com.pennymacusa.excel-formulas.core-test
   (:require [clojure.test :refer :all]
+            [clj-time.format :as f]
             [com.pennymacusa.excel-formulas.core :refer :all]))
 
 (deftest calculating-days-between-dates-in-360-day-year
   (testing "days360"
     (testing "when the start-date and end-date are in the middle of a month"
-      (is (= (days360 "2008-01-30" "2008-02-01") 1))
-      (is (= (days360 "2008-01-01" "2008-02-01") 30)))
+      (is (= (days360 (f/parse "2008-01-30") (f/parse "2008-02-01")) 1))
+      (is (= (days360 (f/parse "2008-01-01") (f/parse "2008-02-01")) 30)))
     (testing "when the start-date is at the end of the month"
-      (is (= (days360 "2015-01-31" "2015-02-01") 1))
-      (is (= (days360 "1996-02-29" "1996-03-01") 1))
-      (is (= (days360 "1993-02-28" "1993-03-01") 1))
-      (is (= (days360 "2008-02-29" "2008-03-30") 30)))
+      (is (= (days360 (f/parse "2015-01-31") (f/parse "2015-02-01")) 1))
+      (is (= (days360 (f/parse "1996-02-29") (f/parse "1996-03-01")) 1))
+      (is (= (days360 (f/parse "1993-02-28") (f/parse "1993-03-01")) 1))
+      (is (= (days360 (f/parse "2008-02-29") (f/parse "2008-03-30")) 30)))
     (testing "when the start-date and end-date are at the end of the month"
-      (is (= (days360 "2008-02-29" "2008-03-31") 30))
-      (is (= (days360 "2008-02-29" "2008-08-31") 180)))
+      (is (= (days360 (f/parse "2008-02-29") (f/parse "2008-03-31")) 30))
+      (is (= (days360 (f/parse "2008-02-29") (f/parse "2008-08-31")) 180)))
     (testing "when the end-date is on the 31st of the month"
-      (is (= (days360 "2008-01-30" "2008-01-31") 0))
-      (is (= (days360 "2008-02-28" "2008-03-31") 33))
-      (is (= (days360 "2015-01-01" "2015-01-31") 30))
-      (is (= (days360 "2008-01-01" "2008-12-31") 360)))))
+      (is (= (days360 (f/parse "2008-01-30") (f/parse "2008-01-31")) 0))
+      (is (= (days360 (f/parse "2008-02-28") (f/parse "2008-03-31")) 33))
+      (is (= (days360 (f/parse "2015-01-01") (f/parse "2015-01-31")) 30))
+      (is (= (days360 (f/parse "2008-01-01") (f/parse "2008-12-31")) 360)))))

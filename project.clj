105b3c78fa9da41e7289eb239747d3f439ab22c9
Daniel Dyba <daniel.dyba@gmail.com>
2015-04-09 12:06:10 -0700

    Version 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.pennymacusa/excel-formulas "0.2.2-SNAPSHOT"
+(defproject com.pennymacusa/excel-formulas "0.2.2"
   :description "Excel formulas ported to Clojure"
   :url "https://github.com/pennymac/excel-formulas"
   :license {:name "Eclipse Public License"

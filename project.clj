989a65e764929bd4d1238528f60cab983a94e9d9
Daniel Dyba <daniel.dyba@gmail.com>
2015-04-09 12:05:29 -0700

    Deploy releases to clojars by default

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/pennymac/excel-formulas"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [clj-time "0.9.0"]]
   :release-tasks [["vcs" "assert-committed"]
